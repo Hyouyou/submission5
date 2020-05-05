8265d6a72cc11c9a82b78c2a947f1e3b1d78f564
vagrant <vagrant@localhost.localdomain>
2020-05-05 08:56:12 +0000

    Add follow-function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_04_102429) do
+ActiveRecord::Schema.define(version: 2020_05_05_063714) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
